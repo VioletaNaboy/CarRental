effd0b39674cbdac7c2865eb40937754c9753d8f
VioletaNaboy <121202853+VioletaNaboy@users.noreply.github.com>
2023-11-17 02:24:07 +0000

    Deploying to gh-pages from @ VioletaNaboy/CarRental@3170acf87468beb0226b14b2743a9f3b2d29795b 🚀

---
+++ assets/Favorites-13b09318.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-19043277.js";const s=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"Favorites"}),e.jsx("p",{children:"Welcome to the home page!"})]});export{s as default};
