9ff6ae062d5555ea695d106cc278457d4db9baee
Violeta <vnaboishchykova@gmail.com>
2023-11-17 02:44:11 +0100

    fixed_deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/CarRental">
+    <BrowserRouter basename="/CarRental/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
