1ad18ba852571c7c1f6ca2c6127c8c31918c98af
Violeta <vnaboishchykova@gmail.com>
2023-11-20 19:00:52 +0100

    added_homepage_content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import mainPhoto from '../img/main.jpg';
+import { ImgMain, ContainerMain, TitleMain, SubtitleMain, ListContainer, LabelLi, StyledUl } from './Home.styled'
+
 
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>Welcome to the home page!</p>
+      <TitleMain>CarRental Ukraine</TitleMain>
+      <SubtitleMain>We are a Ukrainian car rental service!</SubtitleMain>
+      <ContainerMain>
+      <ImgMain src={mainPhoto} alt="Home page photo" />
+      <ListContainer>
+        <h3>How it works:</h3>
+      <StyledUl>
+        <li><LabelLi >Choose What Suits You:</LabelLi> Browse our cars and select what fits your needs.</li>
+        <li><LabelLi >Add to Favorites:</LabelLi> Save cars you like in your "Favorites" and review them from any device.</li>
+        <li><LabelLi >Book Online:</LabelLi> Easily and quickly book your favorite car through our website.</li>
+        <li><LabelLi >Pick Up and Enjoy:</LabelLi> Embark on your journey in style and comfort. Your adventure starts now!</li>
+        </StyledUl>
+      </ListContainer>
+      </ContainerMain>
     </div>
   );
 };
---
+++ src/pages/Home.styled.jsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+export const ImgMain = styled.img`
+width: 100%;
+height: auto;
+border-radius: 24px;
+@media only screen and (min-width: 768px) {
+width: 50%;
+}
+`
+export const ContainerMain = styled.div`
+display:flex;
+flex-direction: column;
+gap: 24px;
+justify-content: center;
+align-items:center;
+@media only screen and (min-width: 768px) {
+flex-direction: row;
+}
+`
+
+export const TitleMain = styled.h1`
+color: #3470FF;
+font-size: 56px;
+margin: 56px 0 32px auto;
+`
+
+export const SubtitleMain = styled.h2`
+font-size: 44px;
+text-transform: uppercase;
+margin: 56px 0 74px;
+`
+
+export const ListContainer = styled.div`
+text-transform: uppercase;
+font-size: 32px;
+`
+export const LabelLi= styled.span`
+color: #3470FF;
+ font-weight: 700;
+font-size: 42px;
+`
+export const StyledUl = styled.ul` 
+display:flex;
+flex-direction: column;
+gap:24px;
+`
