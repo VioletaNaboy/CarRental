d3a61e6e514cb97d45437f0386a873f9e143c608
VioletaNaboy <121202853+VioletaNaboy@users.noreply.github.com>
2023-11-20 18:01:39 +0000

    Deploying to gh-pages from @ VioletaNaboy/CarRental@1ad18ba852571c7c1f6ca2c6127c8c31918c98af 🚀

---
+++ assets/Home-05053f3d.js
@@ -0,0 +1,36 @@
+import{s as n,j as e}from"./index-3f77a438.js";const s="/CarRental/assets/main-11ad449a.jpg",r=n.img`
+width: 100%;
+height: auto;
+border-radius: 24px;
+@media only screen and (min-width: 768px) {
+width: 50%;
+}
+`,t=n.div`
+display:flex;
+flex-direction: column;
+gap: 24px;
+justify-content: center;
+align-items:center;
+@media only screen and (min-width: 768px) {
+flex-direction: row;
+}
+`,o=n.h1`
+color: #3470FF;
+font-size: 56px;
+margin: 56px 0 32px auto;
+`,a=n.h2`
+font-size: 44px;
+text-transform: uppercase;
+margin: 56px 0 74px;
+`,l=n.div`
+text-transform: uppercase;
+font-size: 32px;
+`,i=n.span`
+color: #3470FF;
+ font-weight: 700;
+font-size: 42px;
+`,c=n.ul` 
+display:flex;
+flex-direction: column;
+gap:24px;
+`,x=()=>e.jsxs("div",{children:[e.jsx(o,{children:"CarRental Ukraine"}),e.jsx(a,{children:"We are a Ukrainian car rental service!"}),e.jsxs(t,{children:[e.jsx(r,{src:s,alt:"Home page photo"}),e.jsxs(l,{children:[e.jsx("h3",{children:"How it works:"}),e.jsxs(c,{children:[e.jsxs("li",{children:[e.jsx(i,{children:"Choose What Suits You:"})," Browse our cars and select what fits your needs."]}),e.jsxs("li",{children:[e.jsx(i,{children:"Add to Favorites:"}),' Save cars you like in your "Favorites" and review them from any device.']}),e.jsxs("li",{children:[e.jsx(i,{children:"Book Online:"})," Easily and quickly book your favorite car through our website."]}),e.jsxs("li",{children:[e.jsx(i,{children:"Pick Up and Enjoy:"})," Embark on your journey in style and comfort. Your adventure starts now!"]})]})]})]})]});export{x as default};
